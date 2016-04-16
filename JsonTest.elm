788a8905878e94dd6a9e748cbb14e99d7e815be6
James Wilson <me@unbui.lt>
2016-04-16 10:30:02 +0100

    Clean layout up slightly, indicate when request is loading

--- JsonTest.elm
+++ JsonTest.elm
@@ -18,14 +18,15 @@ import Html.Events exposing (..)
 type alias Response = Maybe (Result Http.RawError Http.Response)
 
 type alias Model =
-    { path   : String
-    , req    : String
-    , verb   : String
-    , res    : Response
+    { path    : String
+    , req     : String
+    , verb    : String
+    , res     : Response
+    , loading : Bool
     }
 
 init : (Model, Effects Action)
-init = (Model "" "" "GET" Nothing, Effects.none)
+init = (Model "" "" "GET" Nothing False, Effects.none)
 
 --
 -- Update: react to events to update our model
@@ -51,11 +52,11 @@ update action model =
     ReqBody str ->
         noFx { model | req = str }
     ResSuccess res ->
-        noFx { model | res = Just (Ok res) }
+        noFx { model | loading = False, res = Just (Ok res) }
     ResError err ->
-        noFx { model | res = Just (Err err) }
+        noFx { model | loading = False, res = Just (Err err) }
     Submit ->
-        (model, makeRequestGiven model)
+        ({model | loading = True, res = Nothing }, makeRequestGiven model)
 
 makeRequestGiven : Model -> Effects Action
 makeRequestGiven model =
@@ -83,7 +84,7 @@ makeRequestGiven model =
 view : Address Action -> Model -> Html
 view address model =
     div [ class "container" ]
-        [ div
+        [ table
             [ class "inputs" ]
             [ row "Path" "input-path" <|
                 input
@@ -100,19 +101,30 @@ view address model =
             ]
         , div
             [ class "submit" ]
-            [ button [ onClick address Submit ] [ text "Submit" ] ]
+            [ button
+                [ onClick address Submit ]
+                [ text "Submit" ]
+            ]
         , div
-            [ class "response" ]
+            [ class "progress", hidden (not model.loading) ]
+            [ text "Fetching Response..." ]
+        , div
+            [ class "response", hidden (not <| isJust model.res) ]
             [ viewResponse model.res ]
         ]
 
+isJust : Maybe a -> Bool
+isJust m = case m of
+    Nothing -> False
+    _ -> True
+
 row : String -> String -> Html -> Html
 row name id input =
-    div [ class "row" ]
-        [ div
+    tr [ class "row" ]
+        [ td
             [ class "row-name"  ]
             [ label [ for id ] [ text name ] ]
-        , div
+        , td
             [ class "row-input" ]
             [ input ]
         ]
@@ -125,9 +137,9 @@ viewResponse res =
     responseError =
         text "There was a low level error processing your request."
     response details =
-        div []
+        table []
             [ row "Status" ""  <| text (toString details.status ++ ": " ++ details.statusText)
-            , row "Headers" "" <| div [] <| List.map viewHeader <| Dict.toList details.headers
+            , row "Headers" "" <| table [] <| List.map viewHeader <| Dict.toList details.headers
             , row "Body" ""    <| viewBody details.value
             ]
     viewHeader (name, value) =
